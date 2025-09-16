12468106c3bd0f28d23c1a8f963bb1466eeafbff
Logesh S <logeshlogesh2004@gmail.com>
2025-09-16 10:55:58 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+<script>
+  var typed = new Typed('#typed-text', {
+    strings: ["Java Developer", "Full Stack Enthusiast", "Problem Solver"],
+    typeSpeed: 50,  // Speed of typing
+    backSpeed: 30,  // Speed of deleting
+    loop: true      // Loop the animation
+  });
+</script>
+
